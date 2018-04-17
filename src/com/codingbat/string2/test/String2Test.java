dd1d2829f0411ce7b15f15d9e99f33c33ad8d24c
mtaskeen <mtaskeen@wks-mtaskeen1.elogex.com>
2018-04-17 15:15:59 -0500

    Initial commit

--- src/com/codingbat/string2/test/String2Test.java
+++ src/com/codingbat/string2/test/String2Test.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 import com.codingbat.string2.src.DoubleChar;
+import com.codingbat.string2.src.OneTwo;
 import com.codingbat.string2.src.SameStarChar;
 
 class String2Test {
@@ -42,6 +43,27 @@ class String2Test {
 		assertFalse(SameStarChar.sameStarChar("12*2*3*"));
 		assertTrue(SameStarChar.sameStarChar("*"));
 		assertTrue(SameStarChar.sameStarChar("**"));
+	}
+	
+	@Test
+	void testOneTwo() {
+		assertEquals("bca", OneTwo.oneTwo("abc"));
+		assertEquals("cat", OneTwo.oneTwo("tca"));
+		assertEquals("catdog", OneTwo.oneTwo("tcagdo"));
+		assertEquals("hocolctea", OneTwo.oneTwo("chocolate"));
+		assertEquals("231564897", OneTwo.oneTwo("1234567890"));
+		assertEquals("abxabxabxabxabxabxabx", OneTwo.oneTwo("xabxabxabxabxabxabxab"));
+		assertEquals("bcaefd", OneTwo.oneTwo("abcdefx"));
+		assertEquals("bcaefd", OneTwo.oneTwo("abcdefxy"));
+		assertEquals("bcaefdyzx", OneTwo.oneTwo("abcdefxyz"));
+		assertEquals("", OneTwo.oneTwo(""));
+		assertEquals("", OneTwo.oneTwo("x"));
+		assertEquals("", OneTwo.oneTwo("xy"));
+		assertEquals("yzx", OneTwo.oneTwo("xyz"));
+		assertEquals("bcaefdhigkljmnkpqostrvwuyzx231564897", OneTwo.oneTwo("abcdefghijklkmnopqrstuvwxyz1234567890"));
+		assertEquals("bcaefdhigkljmnkpqostrvwuyzx231564897", OneTwo.oneTwo("abcdefghijklkmnopqrstuvwxyz123456789"));
+		assertEquals("bcaefdhigkljmnkpqostrvwuyzx231564", OneTwo.oneTwo("abcdefghijklkmnopqrstuvwxyz12345678"));
+		
 	}
 	
 }
