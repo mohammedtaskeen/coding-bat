559d3feee18a9e2a862fea9bd5eaa9ab4fefd49b
mohammedtaskeen <mohammedtaskeen@gmail.com>
2018-04-24 14:58:31 -0500

    Touch

--- src/com/codingbat/string2/src/PlusOut.java
+++ src/com/codingbat/string2/src/PlusOut.java
@@ -2,7 +2,13 @@ package com.codingbat.string2.src;
 
 public class PlusOut {
 
-	
+	/**
+	 * Given a string and a non-empty word string, return a version of the original String 
+	 * where all chars have been replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+	 * plusOut("12xy34", "xy") -> "++xy++"
+	 * plusOut("12xy34", "1") -> "1+++++"
+	 * plusOut("12xy34xyabcxy", "xy") -> "++xy++xy+++xy"
+	 */
 	public static String plusOut(String str, String word) {
 		  if(str==null||str.length()<word.length()) return str;
 		  String plusOut="";
--- src/com/codingbat/string2/src/WordEnds.java
+++ src/com/codingbat/string2/src/WordEnds.java
@@ -2,6 +2,14 @@ package com.codingbat.string2.src;
 
 public class WordEnds {
 
+	/**
+	 * Given a string and a non-empty word string, return a string made of each char just before 
+	 * and just after every appearance of the word in the string. 
+	 * Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
+	 * wordEnds("abcXY123XYijk", "XY") -> "c13i"
+	 * wordEnds("XY123XY", "XY") -> "13"
+	 * wordEnds("XY1XY", "XY") -> "11"
+	 */
 	public static String wordEnds(String str, String word) {
 		  int slen = str.length();
 		  int wlen = word.length();
