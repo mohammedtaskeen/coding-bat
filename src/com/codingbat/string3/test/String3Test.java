843a7a777976554e3863f1b5e564a96018a32854
mohammedtaskeen <mohammedtaskeen@gmail.com>
2018-04-24 16:52:43 -0500

    Solution for String3.WithoutString

---
+++ src/com/codingbat/string3/test/String3Test.java
@@ -0,0 +1,34 @@
+package com.codingbat.string3.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.codingbat.string3.src.WithoutString;
+
+public class String3Test {
+
+	@Test
+	void testWithoutString() {
+		assertEquals("He there", WithoutString.withoutString("Hello there", "llo"));
+		assertEquals("Hllo thr", WithoutString.withoutString("Hello there", "e"));
+		assertEquals("Hello there", WithoutString.withoutString("Hello there", "x"));
+		assertEquals("Th  a FH", WithoutString.withoutString("This is a FISH", "IS"));
+		assertEquals("TH  a FH", WithoutString.withoutString("THIS is a FISH", "is"));
+		assertEquals("TH  a FH", WithoutString.withoutString("THIS is a FISH", "iS"));
+		assertEquals("abab", WithoutString.withoutString("abxxxxab", "xx"));
+		assertEquals("abxab", WithoutString.withoutString("abxxxab", "xx"));
+		assertEquals("abab", WithoutString.withoutString("abxxxab", "x"));
+		assertEquals("", WithoutString.withoutString("xxx", "x"));
+		assertEquals("x", WithoutString.withoutString("xxx", "xx"));
+		assertEquals("xzz", WithoutString.withoutString("xyzzy", "Y"));
+		assertEquals("", WithoutString.withoutString("", "x"));
+		assertEquals("acac", WithoutString.withoutString("abcabc", "b"));
+		assertEquals("AAbb", WithoutString.withoutString("AA22bb", "2"));
+		assertEquals("", WithoutString.withoutString("1111", "1"));
+		assertEquals("", WithoutString.withoutString("1111", "11"));
+		assertEquals("jtx", WithoutString.withoutString("MkjtMkx", "Mk"));
+		assertEquals("Hi ", WithoutString.withoutString("Hi HoHo", "Ho"));
+	}
+	
+}
