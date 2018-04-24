b627b7f1fd265e465bb093645977904152c5c685
mohammedtaskeen <mohammedtaskeen@gmail.com>
2018-04-24 14:45:58 -0500

    solution for String2.WordEnds problem

---
+++ src/com/codingbat/string2/src/WordEnds.java
@@ -0,0 +1,20 @@
+package com.codingbat.string2.src;
+
+public class WordEnds {
+
+	public static String wordEnds(String str, String word) {
+		  int slen = str.length();
+		  int wlen = word.length();
+		  String fin = "";
+		  
+		  for (int i = 0; i < slen-wlen+1; i++) { 
+		    String tmp = str.substring(i,i+wlen);
+		    if (i > 0 && tmp.equals(word))
+		      fin += str.substring(i-1,i);
+		    if (i < slen-wlen && tmp.equals(word))
+		      fin += str.substring(i+wlen,i+wlen+1);
+		  }
+		  return fin;
+		}
+
+}
--- src/com/codingbat/string2/test/String2Test.java
+++ src/com/codingbat/string2/test/String2Test.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 
 import com.codingbat.string2.src.DoubleChar;
 import com.codingbat.string2.src.OneTwo;
+import com.codingbat.string2.src.PlusOut;
 import com.codingbat.string2.src.SameStarChar;
 import com.codingbat.string2.src.StarOut;
+import com.codingbat.string2.src.WordEnds;
 import com.codingbat.string2.src.ZipZap;
 
 class String2Test {
@@ -104,5 +106,28 @@ class String2Test {
 		assertEquals("", StarOut.starOut("*a"));
 		assertEquals("", StarOut.starOut("*"));
 		assertEquals("", StarOut.starOut(""));
+	}
+	
+	@Test
+	void testPlusOut() {
+		assertEquals("++XY+++XY+", PlusOut.plusOut("abXYabcXYZ", "XY"));
+	}
+	
+	
+	@Test
+	void testWordEnds()	{
+		assertEquals("c13i",WordEnds.wordEnds("abcXY123XYijk", "XY"));
+		assertEquals("13",WordEnds.wordEnds("XY123XY", "XY"));
+		assertEquals("11",WordEnds.wordEnds("XY1XY", "XY"));
+		assertEquals("XY",WordEnds.wordEnds("XYXY", "XY") );
+		assertEquals("",WordEnds.wordEnds("XY", "XY"));
+		assertEquals("",WordEnds.wordEnds("Hi", "XY"));
+		assertEquals("",WordEnds.wordEnds("", "XY"));
+		assertEquals("cxziij",WordEnds.wordEnds("abc1xyz1i1j", "1"));
+		assertEquals("cxz",WordEnds.wordEnds("abc1xyz1", "1"));
+		assertEquals("cxz11",WordEnds.wordEnds("abc1xyz11", "1"));
+		assertEquals("11",WordEnds.wordEnds("abc1xyz1abc", "abc"));
+		assertEquals("acac",WordEnds.wordEnds("abc1xyz1abc", "b"));
+		assertEquals("1111",WordEnds.wordEnds("abc1abc1abc", "abc"));
 	}
 }
