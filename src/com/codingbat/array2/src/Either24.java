3c2ea8cbe7057c7b54103465dc68c786bd107201
mtaskeen <mtaskeen@lap-mtaskeen.elogex.com>
2018-07-31 14:17:36 -0500

    Updated

--- src/com/codingbat/array2/src/Either24.java
+++ src/com/codingbat/array2/src/Either24.java
@@ -1,6 +1,6 @@
 package com.codingbat.array2.src;
 
-public class Either24 { 
+public class Either24 {
 
 	/**
 	 * Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
